8924af209cbf3c78649e97bdf6437e8f11cca237
liel.k <liel.k@meuhedet.co.il>
2018-03-15 13:15:09 +0200

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { QuotesService } from './quotes.service';
+import {
+  trigger,
+  state,
+  animate,
+  transition,
+  style
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('itemAnim', [
+      transition(':enter', [
+        style({ transform: 'translateY(20%)', opacity: 0 }),
+        animate(500)
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(20%)', opacity: 0 }),
+        animate(500)
+      ])
+    ]),
+    trigger('itemAnim2', [
+      transition(':enter', [
+        style({ transform: 'translateY(20%)', opacity: 0 }),
+        animate(1000)
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(20%)', opacity: 0 }),
+        animate(1000)
+      ])
+    ]),
+    trigger('itemAnim3', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate(1500)
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate(1500)
+      ])
+    ]),
+    trigger('itemAnim4', [
+      transition(':enter', [
+        style({ transform: 'translateY(10%)', opacity: 0 }),
+        animate(2000)
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(10%)', opacity: 0 }),
+        animate(2000)
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   loaded = false;
