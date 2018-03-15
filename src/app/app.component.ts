e205304503a829b7e6294033907e1a2d84db4ceb
liel.k <liel.k@meuhedet.co.il>
2018-03-15 10:23:54 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,23 @@ import { QuotesService } from './quotes.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  loaded = false;
   quote: string;
   author: string;
   picSrc: string;
   constructor(private quoteService: QuotesService) {}
 
   ngOnInit() {
-    this.quoteService.getRandomQuote().subscribe(val => {
-      console.log(val);
-      this.quote = val['contents'].quote;
-      this.author = val['contents'].author;
-    });
-    this.picSrc = `../assets/pics/${Math.floor(Math.random() * 31 + 1)}.jpg`;
+    this.quoteService.getRandomQuote().subscribe(
+      val => {
+        this.quote = val['contents'].quote;
+        this.author = val['contents'].author;
+        this.picSrc = `../assets/pics/19.jpg`;
+      },
+      () => {},
+      () => setTimeout(() => (this.loaded = true), 1500)
+    );
+
+    // this.picSrc = `../assets/pics/${Math.floor(Math.random() * 31 + 1)}.jpg`;
   }
 }
